3b27f3d988dd9b89a8e21953f57efd233b8a8c74
bloopgoop <zhu.kevin12@gmail.com>
2024-07-27 17:00:54 -0400

    add prisma adapter

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth/next";
 import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: {
