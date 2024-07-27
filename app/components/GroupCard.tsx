3110f7b63899d6a00466ab2d572caca85172b4e6
kenyounot123 <kenlu519@gmail.com>
2024-07-27 17:29:27 -0400

    Update GroupCard component and rename route to dashboard

--- app/components/GroupCard.tsx
+++ app/components/GroupCard.tsx
@@ -1,3 +1,26 @@
-export default function GroupCard() {
-  
+import Image from "next/image"
+
+interface GroupCardProps {
+  groupName: string;
+  groupImgSrc: string;
+}
+export default function GroupCard({groupName, groupImgSrc}: GroupCardProps) {
+  return (
+    <button className="w-full relative">
+      <h2 className="text-left rounded-t-md bg-primary-accent-color text-primary-text-color p-1 text-xl">{groupName}</h2>
+      <div className="h-32">
+        <Image 
+         src={groupImgSrc}
+         width={0}
+         height={0}
+         alt="Picture of the author"
+         sizes="100vw"
+         className="w-full h-full object-cover rounded-b-md" />
+      </div>
+
+      <div>
+
+      </div>
+    </button>
+  )
 }
