50757e7f04fadbd5b9663a3a587ef3b96b20310c
John Doe <patrickrgt@gmail.com>
2024-07-28 00:22:33 -0400

    animated landing

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Landing() {
   return (
     <div className="flex flex-col lg:flex-row w-[80%] m-auto lg:h-screen py-[1vh]">
       <div className="h-full lg:my-auto mx-auto flex flex-col lg:justify-center">
-        <h1 className="text-[6vh] lg:text-[7vh] text-primary-text-color font-[900]">
+        <h1 className="animate-fadeIn text-[6vh] lg:text-[7vh] text-primary-text-color font-[900]">
           Connect, plan, and experience. All in one place.
         </h1>
         <div className="w-[95%] lg:w-[61%] text-primary-accent-color font-bold -ml-[2vh] lg:ml-0 my-5">
-          <h2 className="text-[10vh] lg:text-[20vh] tracking-[-0.1em] font-[900] lg:pl-5">
+          <h2 className="animate-fadeIn text-[10vh] lg:text-[20vh] tracking-[-0.1em] font-[900] lg:pl-5">
             10,000
           </h2>
-          <p className="text-right text-[3.25vh] lg:text-[4vh] font-[900] -mr-[2vh] lg:mr-0">users are Dawdling</p>
+          <p className="animate-fadeIn text-right text-[3.25vh] lg:text-[4vh] font-[900] -mr-[2vh] lg:mr-0">users are Dawdling</p>
         </div>
         <div className="w-[80%] lg:my-0 lg:mx-0 mx-auto flex flex-col gap-3 my-5 pb-5">
           <p className="text-primary-text-color lg:self-start self-end underline underline-offset-2 ">
@@ -19,17 +19,17 @@ export default function Landing() {
           </p>
           <div className="flex lg:flex-row flex-col justify-start lg:w-[35%] lg:gap-[2vh] gap-[1vh]">
             <input
-              className="outline-none transition-all ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black rounded-full py-5 bg-secondary-accent-color"
+              className="animate-fadeIn outline-none transition-all ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black rounded-full py-5 bg-secondary-accent-color"
               type="text"
               placeholder="Email"
             />
             <input
-              className="outline-none transition-all ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black rounded-full py-5 bg-secondary-accent-color"
+              className="animate-fadeIn outline-none transition-all ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black rounded-full py-5 bg-secondary-accent-color"
               type="password"
               placeholder="Password"
             />
             <button
-              className="self-end bg-primary-accent-color text-primary-text-color w-min py-3 px-5 rounded-full"
+              className="animate-fadeIn self-end bg-primary-accent-color text-primary-text-color w-min py-3 px-5 rounded-full"
               type="submit"
             >
               submit
@@ -38,11 +38,11 @@ export default function Landing() {
         </div>
       </div>
 
-      <div className="w-[80%] lg:my-auto mx-auto rotate-[-2deg]">
+      <div className="animation-3d w-[80%] lg:my-auto mx-auto rotate-[-2deg]">
         <h2 className="text-center font-[900] text-2xl text-primary-accent-color my-2">
           27th Saturday 5:00PM
         </h2>
-        <div className="w-[90%] mx-auto relative">
+        <div className="w-[90%] mx-auto relative ">
           <div className="absolute bg-primary-accent-color py-2 px-3 rounded-full top-0 left-0 -translate-x-5 -translate-y-2">
             <div className="flex gap-1">
               <div className="rounded-full bg-white p-1">🦆</div>
@@ -59,7 +59,7 @@ export default function Landing() {
             height={0}
             alt="Picture of the author"
             sizes="100vw"
-            className="w-full h-auto rounded-xl"
+            className=" w-full h-auto rounded-xl"
           />
           </div>
         
