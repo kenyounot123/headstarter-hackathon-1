68eab844e51a23f3c41fe9992a1d7958e8b18509
kenyounot123 <kenlu519@gmail.com>
2024-07-28 00:17:16 -0400

    Refactor code to use html time input and create function to format date time

--- app/events/create/page.tsx
+++ app/events/create/page.tsx
@@ -6,52 +6,30 @@ import { useState } from "react";
 
 
 export default function EventCreate() {
-  const [hours, setHours] = useState<number | ''>('')
-  const [minutes, setMinutes] = useState<number | ''>('')
-  const [period, setPeriod] = useState<'AM' | 'PM'>('AM')
+
   const [date, setDate] = useState<string>("")
+  const [time, setTime] = useState<string>("")
 
   const handleDateChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     setDate(e.target.value)
   }
-
-  const handleHoursChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-    if (value === '' || /^[0-9]{1,2}$/.test(value)) {
-      setHours(Number(value) || '');
-    }
-  }
-
-  const handleMinutesChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value === '' || /^[0-9]{1,2}$/.test(value)) {
-      setMinutes(Number(value) || '');
-    }
-  }
-  const handlePeriodChange = (e:React.MouseEvent<HTMLButtonElement>,newPeriod: 'AM' | 'PM') => {
-    e.preventDefault();
-    setPeriod(newPeriod);
-  };
-
-  function createDateTimeObject(date: string, hours: string, minutes: string, period: 'AM' | 'PM'): Date {
-  
-    // Convert 12-hour clock to 24-hour clock
-    const adjustedHours = period === 'PM' && hours < 12 ? hours + 12 : hours;
-    const finalHours = period === 'AM' && hours === 12 ? 0 : adjustedHours;
-  
-    // Create a new Date object with the provided date and time
-    const dateTime = new Date(`${date}T${String(finalHours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:00`);
-  
-    return dateTime;
+  const handleTimeChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    setTime(e.target.value)
   }
   
+  const createFormattedDateTime = (): Date | null => {
+    if (!date || !time) return null;
 
+    // Construct the datetime string in ISO format
+    const dateTimeString = `${date}T${time}`;
+
+    return new Date(dateTimeString)
+  }
   // Send data to API
   const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault() 
 
-    const formattedTime = ``;
-
+    const formattedTime = createFormattedDateTime();
   }
   return (
     <>
@@ -88,18 +66,11 @@ export default function EventCreate() {
 
           <div className="flex flex-col gap-3">
             <p>Event time</p>
-            <div className="flex justify-between gap-5">
-              <div className="flex gap-1">
-                <input className="text-center max-w-sm w-16 outline-none transition-colors ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color placeholder:text-black bg-secondary-accent-color rounded-full" placeholder="5" type="number" min="1" max="12" value={hours} onChange={handleHoursChange}/>
-                <input className="text-center max-w-sm w-16 outline-none transition-colors ease-in-out duration-300 hover:bg-hover-color p-2 focus:bg-hover-color placeholder:text-black bg-secondary-accent-color rounded-full" placeholder="6" type="number" min="0" max="59" value={minutes} onChange={handleMinutesChange}/>
-              </div>
-              <div className="flex gap-1">
-                <button onClick={(e) => handlePeriodChange(e,"AM")} className={`text-center max-w-sm w-10 outline-none transition-colors ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color placeholder:text-black bg-secondary-accent-color rounded-full`}>AM</button> 
-                <button onClick={(e) => handlePeriodChange(e,"PM")} className="text-center max-w-sm w-10 outline-none transition-colors ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color placeholder:text-black bg-secondary-accent-color rounded-full">PM</button>
-              </div>
+            <div className="flex gap-1">
+              <input className="p-[1vh] text-center outline-none transition-colors ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color placeholder:text-black bg-secondary-accent-color rounded-full" onChange={handleTimeChange} name="time" value={time} type="time"/>
             </div>
           </div>
-
+        
           <div className="flex flex-col gap-3">
             <label htmlFor="category">Event category</label>
             <select className=" transition-colors ease-in-out duration-300 hover:bg-hover-color focus:bg-hover-color px-5 placeholder:text-black bg-secondary-accent-color w-full rounded p-[1vh] outline-none" name="category" id="category">
