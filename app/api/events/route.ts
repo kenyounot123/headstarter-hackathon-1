b27761f6cbd68b1df2ac5107e4298dedae9cfb57
koreanpro22 <dhskim22@gmail.com>
2024-07-28 00:29:41 -0400

    fix check

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,13 +1,13 @@
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 
-export async function getEventUsers(eventId: number) {
-    const users = await prisma.user.findMany({
-        where: {
-            : eventId
-        },
-        include: {
-            users: true
-        }
-    })
-    console.log("✅✅✅ All users => ", users)
-}
+// export async function getEventUsers(eventId: number) {
+//     const users = await prisma.user.findMany({
+//         where: {
+//             : eventId
+//         },
+//         include: {
+//             users: true
+//         }
+//     })
+//     console.log("✅✅✅ All users => ", users)
+// }
