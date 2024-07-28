c28bb514fade056a20a8b836b9785f60ebd7bbdf
kenyounot123 <kenlu519@gmail.com>
2024-07-28 02:02:08 -0400

    Update dashboard page with create group input

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,15 +8,22 @@ export default function Dashboard() {
     <div className="flex flex-col gap-5 w-[80%] mx-auto">
       {/* Email of currently signed in user */}
       <p className="self-end py-5">Email@123.gmail.com</p>
-      <button className="rounded-xl flex justify-center py-5 bg-primary-accent-color text-primary-text-color relative">
-        <p className="text-5xl font-[900]">
-          create <span className="block">group</span>
-        </p>
-        <div className="text-6xl absolute bottom-3 right-7">🦆</div>
+      <button className="rounded-xl flex flex-col items-center justify-center py-7 bg-primary-accent-color text-primary-text-color relative">
+        <div className="w-[70%]">
+          <p className="text-5xl font-[900]">
+            create
+          </p>
+          <input
+              className="outline-none w-full py-2 px-4"
+              type="text"
+              placeholder="group name"
+            />
+        </div>
+        <div className="text-6xl absolute bottom-3 right-3">🦆</div>
       </button>
       <button className="rounded-xl flex justify-center py-5 bg-secondary-accent-color text-primary-accent-color relative">
         <div className="flex flex-col gap-1 items-center w-[70%]">
-          <p className="text-5xl font-[900]">join group</p>
+          <p className="text-5xl font-[900]">join</p>
           <input
             className="outline-none w-full py-2 px-4"
             type="password"
