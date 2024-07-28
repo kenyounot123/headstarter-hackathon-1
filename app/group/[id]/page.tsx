e61e483ce38f21a8e55845bcf45ea850a10637da
kenyounot123 <kenlu519@gmail.com>
2024-07-27 20:52:01 -0400

    Use Link component for redirecting

--- app/group/[id]/page.tsx
+++ app/group/[id]/page.tsx
@@ -1,33 +1,31 @@
 "use client";
 
-import Link from "next/link";
 import GroupEventCard from "../../components/GroupEventCard";
 import { GROUP_OBJ } from "../../lib/global";
 import EventDecisionButton from "../../components/EventChoiceButton";
 import { usePathname, useSearchParams } from "next/navigation";
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   // this works for search params:
   // http://localhost:3004/group/id?123
-  const handleRedirect = () => {
-    window.location.href = "/dashboard";
-  };
 
   return (
     <div className="flex flex-col gap-5 w-[92%] mx-auto">
       <div className="flex w-full m-auto justify-center items-center">
-        <Image
-          alt="eye"
-          src={"/images/arrow-right-solid.svg"}
-          width={0}
-          height={0}
-          className="w-[3.5vh] h-[3.5vh] rotate-180"
-          onClick={() => handleRedirect()}
-        ></Image>{" "}
+        <Link href="/dashboard">
+          <Image
+            alt="eye"
+            src={"/images/arrow-right-solid.svg"}
+            width={0}
+            height={0}
+            className="w-[3.5vh] h-[3.5vh] rotate-180"
+          />
+        </Link>
         <div className="flex w-full justify-between items-center text-center text-[3vh] p-[0.5vh] rounded-[1vh] bg-primary-accent-color">
           <h1 className="text-primary-text-color">{searchParams}</h1>
           <div>
