a734d132d1763e79087021198c3b71cbf330c5da
bloopgoop <zhu.kevin12@gmail.com>
2024-07-27 23:41:45 -0400

    add users api

---
+++ app/api/users/route.tsx
@@ -0,0 +1,50 @@
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth/next";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+
+export async function GET(req: Request) {
+    /**
+     * Gets all information about user
+     */
+    // const data = await req.formData()
+    
+    const url = new URL(req.url);
+    const queryParams = url.searchParams;
+
+    // Example: Get the value of the "email" query parameter
+    const email = queryParams.get("email");
+  
+    try {
+      // find group with group id and user email
+      const user = await prisma.user.findUnique({
+        where: {
+          email: email,
+        },
+        include: {
+            groups: {
+                select: {
+                    id: true,
+                    adminId: true,
+                    secretCode: true,
+                    createdAt: true,
+                }
+            },
+            events: {
+                select: {
+                    id: true,
+                    jobId: true,
+                    adminId: true,
+                    groupId: true,
+                    createdAt: true,
+                }
+            }
+        }
+        });
+
+      return NextResponse.json(user);
+    } catch (error) {
+      console.error("Error fetching users in group:", error);
+      throw error;
+    }
+  }
