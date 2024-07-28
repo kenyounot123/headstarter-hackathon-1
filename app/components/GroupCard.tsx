f74e94b04cc166269b32b708d1ec023d7d8d654f
kenyounot123 <kenlu519@gmail.com>
2024-07-27 21:46:17 -0400

    Create event route for event creation

--- app/components/GroupCard.tsx
+++ app/components/GroupCard.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
+import Link from "next/link";
 
 interface GroupProps {
+  id: number;
   eventName: string;
   eventDate: string;
   eventTime: string;
@@ -16,21 +18,22 @@ interface Group {
 
 export default function GroupCard({ group }: Group) {
   return (
-    <button className="w-full relative">
-      <h2 className="text-left rounded-t-md bg-primary-accent-color text-primary-text-color p-1 text-xl">{group.eventName}</h2>
-      <div className="h-32">
-        <Image 
-         src={group.groupImgSrc}
-         width={0}
-         height={0}
-         alt="Picture of the author"
-         sizes="100vw"
-         className="w-full h-full object-cover rounded-b-md" />
-      </div>
-
-      <div className="bg-primary-accent-color py-2 px-3 rounded-full absolute bottom-2 right-2">
-        <p className="text-primary-text-color font-[800]">🦆 {group.eventMembers.length}</p>
-      </div>
-    </button>
+    <Link href={`/group/${group.id}`}>
+      <button className="w-full relative">
+        <h2 className="text-left rounded-t-md bg-primary-accent-color text-primary-text-color p-1 text-xl">{group.eventName}</h2>
+        <div className="h-32">
+          <Image
+           src={group.groupImgSrc}
+           width={0}
+           height={0}
+           alt="Picture of the author"
+           sizes="100vw"
+           className="w-full h-full object-cover rounded-b-md" />
+        </div>
+        <div className="bg-primary-accent-color py-2 px-3 rounded-full absolute bottom-2 right-2">
+          <p className="text-primary-text-color font-[800]">🦆 {group.eventMembers.length}</p>
+        </div>
+      </button>
+    </Link>
   )
 }
---
+++ app/events/create/page.tsx
@@ -0,0 +1,8 @@
+export default function EventCreate() {
+  return (
+    <div className="w-[80%] mx-auto">
+      <h1 className="text-primary-text-color">Create Event</h1>
+      <form action=""></form>
+    </div>
+  )
+}
